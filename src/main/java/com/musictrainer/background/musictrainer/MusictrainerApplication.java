f3de7d609acd75870d6009b1f0d96d0363152230
David <dfennell31@gmail.com>
2020-05-15 12:39:50 -0400

    Initial commit

---
+++ src/main/java/com/musictrainer/background/musictrainer/MusictrainerApplication.java
@@ -0,0 +1,13 @@
+package com.musictrainer.background.musictrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusictrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusictrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musictrainer/background/musictrainer/MusictrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musictrainer.background.musictrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusictrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
