06c141eb0cf5f90aa6b6c21a62965dfbc43c5a2c
David <dfennell31@gmail.com>
2020-06-03 10:28:42 -0400

    Switching PC

--- src/main/java/com/musictrainer/background/musictrainer/users/service/UsersData.java
+++ src/main/java/com/musictrainer/background/musictrainer/users/service/UsersData.java
@@ -17,35 +17,35 @@ public class UsersData {
         this.password = password;
     }
 
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public int getCurrentLevel() {
-//        return currentLevel;
-//    }
-//
-//    public void setCurrentLevel(int currentLevel) {
-//        this.currentLevel = currentLevel;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
+
+    public void setCurrentLevel(int currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
