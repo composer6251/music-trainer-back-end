c3884417fba413502f381d7c152434e8748bf3d6
David <dfennell31@gmail.com>
2020-06-17 07:58:38 -0400

    Working on JWT

---
+++ src/main/java/com/musictrainer/background/musictrainer/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.musictrainer.background.musictrainer.jwt.resource;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
